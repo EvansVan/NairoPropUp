28f8ebc3e26245a4b4a7fd6d8e2f2455c49e083a
EvansVan <evansndiritu@gmail.com>
2025-11-05 12:15:08 +0300

    build check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Router as WouterRouter, Switch, Route } from "wouter";
-import useHashLocation from "wouter/use-hash-location";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
